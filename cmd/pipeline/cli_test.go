fb38cf3b02439ab171a03fa10fc240ddca862de2
linyows <linyows@gmail.com>
2017-04-09 22:53:27 +0900

    add test for cli

---
+++ cmd/pipeline/cli_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestRun_versionFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("pipeline --version", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("expected %d to eq %d", status, ExitCodeOK)
+	}
+
+	expected := fmt.Sprintf("pipeline version %s", Version)
+	if !strings.Contains(outStream.String(), expected) {
+		t.Errorf("expected %q to eq %q", outStream.String(), expected)
+	}
+}
